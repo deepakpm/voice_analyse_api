adbf7cc6d568b15ec99c008cd4ed3a4cc37b35f7
Deepak Murali <deepakpm23@gmail.com>
2021-01-18 15:31:11 +0530

    Aws sdk implementation

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 const app = express();
 
+
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
---
+++ src/services/speech_to_text_service.js
@@ -0,0 +1,86 @@
+const {
+  TranscribeClient,
+  StartTranscriptionJobCommand,
+  ListTranscriptionJobsCommand,
+  ModelStatus,
+} = require("@aws-sdk/client-transcribe");
+const aws = require("aws-sdk");
+
+const config = {
+  REGION: "ap-south-1",
+  params: {
+    TranscriptionJobName: "sample_job",
+    LanguageCode: "en-US",
+    MediaSampleRateHertz: 16000,
+    MediaFormat: "wav",
+  },
+};
+
+const client = new TranscribeClient({ region: config.REGION });
+
+const request_for_transcribe = async (mediaUrl) => {
+  try {
+    const jobName = "voice-analyse-" + Date.now().toString();
+    let data = await client.send(
+      new StartTranscriptionJobCommand({
+        ...config.params,
+        OutputBucketName: "voiceanalysisapp",
+        TranscriptionJobName: jobName,
+        Media: {
+          MediaFileUri: mediaUrl,
+        },
+      })
+    );
+    return Promise.resolve(jobName);
+  } catch (err) {
+    return Promise.reject(`Service error${err.message}`);
+  }
+};
+
+const read_transcription = async (jobName) => {
+  try {
+    const params = {
+      JobNameContains: jobName, // Not required. Returns only transcription
+      // job names containing this string
+    };
+    const response = await client.send(
+      new ListTranscriptionJobsCommand(params)
+    );
+
+    const { TranscriptionJobSummaries } = response;
+    const {
+      TranscriptionJobStatus,
+      TranscriptionJobName,
+    } = TranscriptionJobSummaries[0];
+
+    if (TranscriptionJobStatus === ModelStatus.COMPLETED) {
+      const s3params = {
+        Bucket: "voiceanalysisapp",
+        Key: `${TranscriptionJobName}.json`,
+      };
+      const awsS3 = new aws.S3();
+      return new Promise((resolve, reject) => {
+        awsS3.getObject(s3params, function (err, res) {
+          if (err) {
+            reject(err.message);
+          } else {
+            resolve({
+              status: TranscriptionJobStatus,
+              text: JSON.parse(res.Body.toString()).results.transcripts[0]
+                .transcript,
+            });
+          }
+        });
+      });
+    } else {
+      return Promise.resolve({
+        status: TranscriptionJobStatus,
+        text: "",
+      });
+    }
+  } catch (err) {
+    return Promise.reject(err.message);
+  }
+};
+
+module.exports = { request_for_transcribe, read_transcription };
